2a9437b1f1524550fbbb582690b2a70f7ebe63bc
thanhloc2906 <162592816+thanhloc2906@users.noreply.github.com>
2025-08-09 00:56:20 +0900

    Update fireworks.js

--- assets/fireworks.js
+++ assets/fireworks.js
@@ -4,7 +4,64 @@ import Fireworks from "https://cdn.jsdelivr.net/npm/fireworks-js@2.9.2/dist/fire
 export function startFireworks() {
   const container = document.body;
   const fireworks = new Fireworks(container, {
-    rocketsPoint: { min: 0, max: 100 },
+    rocketsPoint: { min: 0, max: 100 },// Daniel Shiffman
+// http://codingtra.in
+// https://youtu.be/CKeyIbT3vXI
+
+class Firework {
+  constructor() {
+    this.hu = random(255);
+    this.firework = new Particle(random(width), height, this.hu, true);
+    this.exploded = false;
+    this.particles = [];
+  }
+
+  done() {
+    if (this.exploded && this.particles.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  update() {
+    if (!this.exploded) {
+      this.firework.applyForce(gravity);
+      this.firework.update();
+
+      if (this.firework.vel.y >= 0) {
+        this.exploded = true;
+        this.explode();
+      }
+    }
+
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].applyForce(gravity);
+      this.particles[i].update();
+
+      if (this.particles[i].done()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+
+  explode() {
+    for (let i = 0; i < 100; i++) {
+      const p = new Particle(this.firework.pos.x, this.firework.pos.y, this.hu, false);
+      this.particles.push(p);
+    }
+  }
+
+  show() {
+    if (!this.exploded) {
+      this.firework.show();
+    }
+
+    for (var i = 0; i < this.particles.length; i++) {
+      this.particles[i].show();
+    }
+  }
+}
     hue: { min: 0, max: 360 },
     delay: { min: 15, max: 30 },
     speed: 2,
@@ -25,3 +82,4 @@ export function startFireworks() {
 
   fireworks.start();
 }
+
